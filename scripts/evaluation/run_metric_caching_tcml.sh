a40e58d6833902d7554fe77489ac79d38b05e40c
Long Nguyen <long.pollehn@gmail.com>
2025-11-08 08:37:26 +0100

    Update submodule 3rd_party/navsim_workspace/navsimv1.1

--- scripts/evaluation/run_metric_caching_tcml.sh
+++ scripts/evaluation/run_metric_caching_tcml.sh
@@ -12,6 +12,8 @@
 TRAIN_TEST_SPLIT=navtest
 CACHE_PATH=$NAVSIM_EXP_ROOT/metric_cache
 
+export NAVSIM_DEVKIT_ROOT="${PROJECT_DIR}/3rd_party/navsim_workspace/navsimv1.1"
+
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_metric_caching.py \
 train_test_split=$TRAIN_TEST_SPLIT \
 cache.cache_path=$CACHE_PATH
