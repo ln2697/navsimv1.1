3066e23d2ec5baca20bd35d4bcd02a6d05992121
Long Nguyen <long.pollehn@gmail.com>
2025-11-08 09:12:13 +0100

    Update submodule 3rd_party/navsim_workspace/navsimv1.1

--- scripts/evaluation/run_metric_caching_tcml.sh
+++ scripts/evaluation/run_metric_caching_tcml.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00:00
 #SBATCH --gres=gpu:0
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=16
 #SBATCH --partition=L40Sday
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=long.nguyen@student.uni-tuebingen.de
